f2a864d956ee5f3f36b425116be7b9c4623d772a
faezehkochakzadeh <90358136+faezehkochakzadeh@users.noreply.github.com>
2021-09-08 15:49:03 -0700

    Add files via upload

---
+++ calcuateBMI.py
@@ -0,0 +1,8 @@
+#faezeh kuchakzadeh
+#class thursday 14-18
+#calcuate BMI
+Height = float(input('enter your height (m) :\n'))
+Weight = int(input('enter your weight (kg):\n'))
+
+BMI = Weight/Height**2
+print(BMI)
