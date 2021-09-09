cfcb7ce4dc3d1e6028e32288ffbcdf6a3f7d26a7
faezehkochakzadeh <90358136+faezehkochakzadeh@users.noreply.github.com>
2021-09-08 18:03:40 -0700

    Add files via upload

---
+++ s3h1.py
@@ -0,0 +1,13 @@
+#faezeh kuchakzadeh
+#class thursday 14-18
+#calcuate BMI in function (def)
+
+Height = float(input('enter your height (m) :\n'))
+Weight = int(input('enter your weight (kg):\n'))
+
+BMI = Weight/Height**2
+def calcuateBMI (BMI):
+    BMI = Weight/Height**2
+    print(f'Body mass index = {BMI}')
+
+calcuateBMI(BMI)
